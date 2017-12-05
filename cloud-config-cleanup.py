3b4cc5989a488e6e5369d53aeacfc96ce733fce0
Patrick Christopher <coffeepac@gmail.com>
2017-12-04 22:17:52 -0800

    WIP:  cloud configs being re-written
    ToDo:
     - test cloud config files
     - walk through all node types
     - determine best way to call against several worker nodes

--- cloud-config-cleanup.py
+++ cloud-config-cleanup.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
-import argparse
 import re
-import sys
-import os
 
 def clean_start(filepath):
     cloud_config_fd = open(filepath, "r")
@@ -16,6 +13,10 @@ def clean_end(filepath, cloud_config):
     cloud_config_fd.write(cloud_config)
     cloud_config_fd.close()
  
+def install_docker(cloud_config):
+    docker_install = "packages:\n- docker.io"
+    cloud_config += docker_install
+    return cloud_config
 
 def clean_etcd(filepath):
     if filepath is None:
@@ -49,21 +50,53 @@ def clean_master(filepath, ipaddress):
     cloud_config = clean_start(filepath)
 
     #  rule: change dns entry for etcd cluster to passed-in ip address
-    cloud_config = re.sub("etcd[\w\.\-]*internal", ipaddress, cloud_config)
+    cloud_config = re.sub("etcd[" + r"\w\.\-" + "]*internal", ipaddress, cloud_config)
+
+    #  rule:  change occurances of /usr/bin/[mkdir|bash|systemctl] to /bin/[mkdir|bash|systemctl]
+    cloud_config = re.sub("/usr/bin/([mkdir|bash|systemctl])", "/bin/" + r"\g" + "<1>", \
+        cloud_config)
+
+    #  rule:  remove '- --cloud-provider=maas\n' from manifests
+    cloud_config = re.sub("\\\\n[\\\\ \\n]*?-[\\\\ \\n]*?--cloud-provider=maas[ ]*?\\\\n", \
+        "\\\\n", cloud_config, flags=re.S)
+
+    #  rule:  remove '--cloud-provider=maas' from kubelet unit file
+    #  rule:  add '--fail-swap-on=false' to kubelet unit file
+    cloud_config = re.sub("--cloud-provider=maas", "--fail-swap-on=false", cloud_config, count=1)
+
+    #  rule:  install docker
+    cloud_config = install_docker(cloud_config)
+
+    clean_end(filepath, cloud_config)
+
+def clean_worker(filepath, ipaddress):
+    if filepath is None:
+        print "no filepath passed to script.  do nothing"
+        return
+
+    if ipaddress is None:
+        print "ipaddress of master not provided.  this is not going to work"
+        return
+
+    cloud_config = clean_start(filepath)
+
+    #  rule:  replace assumed DNS of cluster (apiserver.<clustername>.internal) with ipaddress
+    cloud_config = re.sub("apiserver.*?internal", ipaddress, cloud_config)
 
     #  rule:  change occurances of /usr/bin/[mkdir|bash|systemctl] to /bin/[mkdir|bash|systemctl]
     cloud_config = re.sub("/usr/bin/([mkdir|bash|systemctl])", "/bin/\g<1>", cloud_config)
 
-    #  rule:  remove '- --cloud-provider=maas\n' from manifests
-    cloud_config = re.sub("\\\\n[\\\\ \\n]*?-[\\\\ \\n]*?--cloud-provider=maas[ ]*?\\\\n", "\\\\n", cloud_config, flags=re.S)
-
     #  rule:  remove '--cloud-provider=maas' from kubelet unit file
-    cloud_config = re.sub("\\\\\\n[\\\\ \n]*?--cloud-provider=maas[\\\\ \n]*?\\\\\\n", "\\\\\\n", cloud_config, flags=re.S|re.DEBUG)
+    #  rule:  add '--fail-swap-on=false' to kubelet unit file
+    cloud_config = re.sub("--cloud-provider=maas", "--fail-swap-on=false", cloud_config, count=1)
+
+    #  rule:  install docker
+    cloud_config = install_docker(cloud_config)
 
     clean_end(filepath, cloud_config)
 
 
 
-
 clean_etcd("/Users/pat/.kraken/charles-fresh/cloud-config/etcd.cloud-config.yaml")
 clean_master("/Users/pat/.kraken/charles-fresh/cloud-config/master.cloud-config.yaml", "10.0.0.0")
+clean_worker("/Users/pat/.kraken/charles-fresh/cloud-config/clusterNodes.cloud-config.yaml", "8.8.8.8")
